47560506039739c7924e70200ba1ba4976d837d8
chjinlook <chjinlook@gmail.com>
2019-07-06 17:38:33 +0900

    How to Handle String input for Integer Fields - Configure Resource Bundle

--- src/com/luv2code/springdemo/mvc/Customer.java
+++ src/com/luv2code/springdemo/mvc/Customer.java
@@ -10,13 +10,14 @@ public class Customer {
 
 	private String firstName;
 	
-	@NotNull
+	@NotNull(message="is required")
 	@Size(min=1, message="is required!")
 	private String lastName;
 	
+	@NotNull(message="is required")
 	@Min(value=0, message="must be greater than or equal to zero")
-	@Max(value=10, message="must be less than or equal to zero")
-	private int freePasses;
+	@Max(value=10, message="must be less than or equal to 10")
+	private Integer freePasses;
 	
 	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
 	private String postalCode;
@@ -45,11 +46,11 @@ public class Customer {
 		this.lastName = lastName;
 	}
 
-	public int getFreePasses() {
+	public Integer getFreePasses() {
 		return freePasses;
 	}
 
-	public void setFreePasses(int freePasses) {
+	public void setFreePasses(Integer freePasses) {
 		this.freePasses = freePasses;
 	}
 	
